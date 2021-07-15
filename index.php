786df6da5f1c034db9711b92ffba0c79ff985b2c
maxnakov <48023859+maxnakov@users.noreply.github.com>
2021-07-15 14:36:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>JobsBG</title>
+    <title>JobsList</title>
     <link rel="stylesheet" type="text/css">
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
