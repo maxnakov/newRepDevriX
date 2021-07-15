6ed8ce5999b3afb6df7c189855305e4b2652f05e
maxnakov <48023859+maxnakov@users.noreply.github.com>
2021-07-15 14:36:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <link rel="stylesheet" type="text/css">
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
 <body>
 <main>
     <div>
@@ -18,4 +17,3 @@
     </div>
 </main>
 </body>
-</html>
