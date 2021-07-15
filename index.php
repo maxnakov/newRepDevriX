c7f3c33f5566ce8ebe2d6f0965890b5ca5bfbff3
maxnakov <48023859+maxnakov@users.noreply.github.com>
2021-07-15 14:34:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>JobsBG</title>
+    <link rel="stylesheet" type="text/css">
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<main>
+    <div>
+        <a href="./index.php">Home</a>
+        <a href="./user-registration.php">Register</a>
+        <a href="./login.php">Login</a>
+    </div>
+    <div class="home">
+        <h1>JobsList</h1>
+    </div>
+</main>
+</body>
+</html>
